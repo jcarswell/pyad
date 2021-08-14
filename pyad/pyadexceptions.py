7b29faf20ad7f0b85e2496f5e16d6e0a17ce5e21
Josh Carswell <jcarswell@ttdev.ca>
2021-08-14 10:47:30 -0600

    Cleaned up exception handling and ensured that __str__ is returing a string and not printing

--- pyad/pyadexceptions.py
+++ pyad/pyadexceptions.py
@@ -1,29 +1,18 @@
-from __future__ import print_function
-from builtins import str
 class comException(Exception):
     def __init__(self, error_info, additional_info={}):
         self.error_info = error_info
-        self.additional_info = additional_info   
-    
+        self.additional_info = additional_info
+
     def __str__(self):
-        print("Error Constant: %s" % self.error_info['error_constant'])
-        print("Error Code: %s" % str(self.error_info['error_code']))
-        #print "Error Message: %s" % self.error_info['error_message']
-        #print "type is ", self.error_info['error_message'].__class__
-        #return "%s (%s): %s" % (str(self.error_info['error_constant']), str(self.error_info['error_code']), str(self.error_info['error_message']))
+        return "%s (%s): %s" % (str(self.error_info['error_constant']), str(self.error_info['error_code']), str(self.error_info['error_message']))
+
 
 class genericADSIException(comException):
-    def __init__(self, error_info, additional_info={}):
-        comException.__init__(error_info, additional_info)
-
     def __str__(self):
         return "%s (%s): %s" % (self.error_info['error_constant'], self.error_info['error_code'], self.error_info['error_message'])
 
 
 class win32Exception(comException):
-    def __init__(self, error_info, additional_info={}):
-        comException.__init__(self, error_info, additional_info)
-
     def __str__(self):
         return "%s: %s" % (self.error_info['error_code'], self.error_info['message'])
 
@@ -39,14 +28,9 @@ class noObjectFoundException(Exception):
 
 
 class InvalidObjectException(noObjectFoundException, win32Exception):
-    def __init__(self, error_info, additional_info):
-        win32Exception.__init__(self, error_info, additional_info)
-
+    pass
 
 class InvalidAttribute(AttributeError):
-    def __init__(self, obj, attribute):
-        self.obj, self.attribute = obj, attribute
-
     def __str__(self):
         return 'The attribute "%s" is not permitted by the schema definition of the object "%s" (the requested attribute does not exist).' % (self.attribute, self.obj)
 
@@ -58,7 +42,7 @@ class noExecutedQuery(Exception):
 
 class invalidResults(Exception):
   def __init__(self, numberResults):
-    self.__numberResults = numberResults
+    self.number_results = numberResults
     
   def __str__(self):
-    return 'The specified query returned %i results. getSingleResults only functions with a single result.' % self.__numberResults
+    return 'The specified query returned %i results. getSingleResults only functions with a single result.' % self.number_results
