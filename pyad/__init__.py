8bd583cc11045480fa99b33f512c566b739b2334
Josh Carswell <jcarswell@ttdev.ca>
2021-08-16 10:31:30 -0600

    Add requirements checking and added imports.

--- pyad/__init__.py
+++ pyad/__init__.py
@@ -1,3 +1,29 @@
-from __future__ import absolute_import
-from .adbase import set_defaults as pyad_setdefaults
-__all__ = ["adbase", "adquery", "adsearch", "adobject", "adcomputer", "adcontainer", "addomain", "adgroup", "aduser", "pyad"]
+
+__all__ = ["set_defaults", "ADQuery", "ADComputer", "ADContainer", "ADDomain", "ADGroup", "ADUser","from_cn","from_dn","from_guid"]
+
+def _check_requirments():
+    import sys
+    import importlib
+    required_version = (3, 6)
+    cont = True
+    msg = "Please ensure the following packages are installed:"
+    if sys.version_info < required_version:
+        raise ImportError("Requires at least Python 3.6")
+    for x in ['win32api','pywintypes','win32com','win32security']:
+        try:
+            importlib.import_module(x)
+        except ModuleNotFoundError:
+            cont = False
+            msg = f"{msg} {x}"
+    if not cont:
+        raise ImportError(msg)
+_check_requirments()
+
+from .adbase import set_defaults
+from .adquery import ADQuery
+from .adcomputer import ADComputer
+from .adcontainer import ADContainer
+from .addomain import ADDomain
+from .adgroup import ADGroup
+from .aduser import  ADUser
+from .pyad import from_cn,from_dn,from_guid
