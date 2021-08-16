c1b336f43d901009303e0ea622df7d930a5105ad
Josh Carswell <jcarswell@ttdev.ca>
2021-08-16 10:30:15 -0600

    Updated setup requirements, git path, stability

--- setup.py
+++ setup.py
@@ -15,17 +15,17 @@ setup(
     author_email = "zakird@gmail.com",
     maintainer = "Zakir Durumeric",
     maintainer_email = "zakird@gmail.com",
-    download_url = "https://github.com/zakird/pyad/",
-    url = "https://zakird.com/pyad",
+    download_url = "https://github.com/jcarswell/pyad/",
     description = "An Object-Oriented Active Directory management framework built on ADSI",
     license = "Apache License, Version 2.0",
     keywords = "python microsoft windows active directory AD adsi",
+    python_requires='>=3.6',
     packages=[
         'pyad'
     ],
     long_description = read('README.rst'),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: System Administrators",
         "Natural Language :: English",
@@ -34,6 +34,6 @@ setup(
     ],
     install_requires=[
         'setuptools',
-        'future'
+        'pywin32'
     ]
 )
