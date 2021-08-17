130a1f99ac9348c47d69823e01af25b84bb23618
Josh Carswell <jcarswell@ttdev.ca>
2021-08-17 07:07:32 -0600

    Seperated out configuration from base testing class

---
+++ pyad/tests/config.py
@@ -0,0 +1,10 @@
+class TestConfiguration:
+    SANDBOX_OU = "ou=testing,dc=example,dc=com"
+    SANDBOX_DOMAIN = "dc=example,dc=com"
+    SANDBOX_FOREST = "dc=example,dc=com"
+    
+    TEST_DC = 'dc01'
+    
+    KNOWN_EXISTS_USER = 'testuset'
+    KNOWN_EXISTS_COMPUTER = 'testcomputer'
+    KNOWN_DNE_OBJECT = "testdne"    
