aebdede7ef56fb2e05fe558871558f16f99d3dda
Josh Carswell <jcarswell@ttdev.ca>
2021-08-17 07:05:55 -0600

    Updated test to use new import model. changed dc check to use config instead of staticly typed

--- pyad/tests/tests_adbase.py
+++ pyad/tests/tests_adbase.py
@@ -1,21 +1,23 @@
 from __future__ import absolute_import
-from .pyadunittest import *
+from .pyadunittest import ADTestCase
+from pyad.adbase import ADBase
+import pyad
 
 class TestADBase(ADTestCase):
     def setUp(self):
         # set all defaults back to their default
-        pyad.adbase.ADBase.default_ldap_server = None
-        pyad.adbase.ADBase.default_gc_server = None
-        pyad.adbase.ADBase.default_ldap_port = None
-        pyad.adbase.ADBase.default_gc_port = None
+        ADBase.default_ldap_server = None
+        ADBase.default_gc_server = None
+        ADBase.default_ldap_port = None
+        ADBase.default_gc_port = None
     
     def test_detected_forest(self):
-        self.assertEqual(pyad.adbase.ADBase.default_domain, self.SANDBOX_DOMAIN)
+        self.assertEqual(ADBase.default_domain, self.SANDBOX_DOMAIN)
     
     def test_detected_domain(self):
-        self.assertEqual(pyad.adbase.ADBase.default_forest, self.SANDBOX_FOREST)
+        self.assertEqual(ADBase.default_forest, self.SANDBOX_FOREST)
     
     def test_set_defaults(self):
-        pyad.adbase.set_defaults(ldap_server = 'iowadc1', ldap_port = 389)
-        self.assertEqual(pyad.adbase.ADBase.default_ldap_server, 'iowadc1')
-        self.assertEqual(pyad.adbase.ADBase.default_ldap_port, 389)
+        pyad.set_defaults(ldap_server = self.TEST_DC, ldap_port = 389)
+        self.assertEqual(ADBase.default_ldap_server, self.TEST_DC)
+        self.assertEqual(ADBase.default_ldap_port, 389)
