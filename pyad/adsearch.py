7564fccc24ba8a974d4c08fb0f66cdeb74e910d1
Josh Carswell <Josh.Carswell@thecarswells.ca>
2022-03-20 11:11:00 -0600

    Cleaned up imports. refactored code to not have a single open query object. Extended ADQuery class to simplify initialization and searching.

--- pyad/adsearch.py
+++ pyad/adsearch.py
@@ -1,79 +1,69 @@
 from __future__ import absolute_import
-from .adquery import *
-from .adbase import * 
+from .adquery import ADQuery
+from .adbase import ADBase 
 
-_ad_query_obj = ADQuery()
+class SearchBase(ADQuery):
+    base_dn = None
 
-def by_cn(cn, search_base=None, options={}):
-    if not search_base:
-        if not ADBase.default_domain:
-            raise Exception("Unable to detect default domain. Must specify search base.")
-        search_base = ADBase.default_domain
-    _ad_query_obj.reset()
-    
-    _ad_query_obj.execute_query(where_clause=("CN = '%s'" % cn), 
-        base_dn=search_base,
-        options=options,
-        type="GC")
-    return _ad_query_obj.get_single_result()['distinguishedName']
+    def __init__(self,search_base:str =None, options:dict =None):
+        super().__init__(options=options or {})
+        if not search_base:
+            if not ADBase.default_domain:
+                raise Exception("Unable to detect default domain. Must specify search base.")
+            self.base_dn = ADBase.default_domain
 
-def by_upn(upn, search_base=None, options={}):
-    if not search_base: 
-        if not ADBase.default_forest:
-            raise Exception("Unable to detect default forest. Must specify search base.")
-        search_base = ADBase.default_forest
-    _ad_query_obj.reset()
-    _ad_query_obj.execute_query(where_clause=("userPrincipalName = '%s'" % upn), 
-        base_dn=search_base, 
-        type="GC", 
-        options=options)
-    return _ad_query_obj.get_single_result()['distinguishedName']
+    def execute_query(self, **kwags) -> None:
+        kwags.setdefault('base_dn',self.base_dn)
+        return super().execute_query(**kwags)
 
-def by_sid(sid, search_base=None, options={}):
-    if not search_base:
-        if not ADBase.default_domain:
-            raise Exception("Unable to detect default domain. Must specify search base.")
-        search_base = ADBase.default_domain
-    _ad_query_obj.reset()
-    _ad_query_obj.execute_query(where_clause=("objectSid = '%s'" % sid), 
-        base_dn=search_base, 
-        options=options, 
-        type="GC")
-    return _ad_query_obj.get_single_result()['distinguishedName']
 
-def all_results_by_cn(cn, search_base=None, options={}):
-    if not search_base:
-        if not ADBase.default_domain:
-            raise Exception("Unable to detect default domain. Must specify search base.")
-        search_base = ADBase.default_domain
-    _ad_query_obj.reset()
-    
-    _ad_query_obj.execute_query(where_clause=("CN = '%s'" % cn), 
-        base_dn=search_base,
-        options=options,
-        type="GC")
-    return [result['distinguishedName'] for result in _ad_query_obj.get_all_results()]
+def by_cn(cn:str, search_base:str =None, options:dict =None):
+    searcher = SearchBase(search_base,options)
+    searcher.execute_query(where_clause=("CN = '%s'" % cn), type="GC")
 
-def all_results_by_upn(upn, search_base=None, options={}):
-    if not search_base: 
-        if not ADBase.default_forest:
-            raise Exception("Unable to detect default forest. Must specify search base.")
-        search_base = ADBase.default_forest
-    _ad_query_obj.reset()
-    _ad_query_obj.execute_query(where_clause=("userPrincipalName = '%s'" % upn), 
-        base_dn=search_base, 
-        type="GC", 
-        options=options)
-    return [result['distinguishedName'] for result in _ad_query_obj.get_all_results()]
+    if searcher.row_count > 0:
+        return searcher.get_single_result()['distinguishedName']
+    else:
+        return []
+
+def by_upn(upn:str, search_base:str =None, options:dict =None):
+    searcher = SearchBase(search_base,options)
+    searcher.execute_query(where_clause=("userPrincipalName = '%s'" % upn), type="GC")
+
+    if searcher.row_count > 0:
+        return searcher.get_single_result()['distinguishedName']
+    else:
+        return []
+
+def by_sid(sid:str, search_base:str =None, options:dict =None):
+    searcher = SearchBase(search_base,options)
+    searcher.execute_query(where_clause=("objectSid = '%s'" % sid), type="GC")
+
+    if searcher.row_count > 0:
+        return searcher.get_single_result()['distinguishedName']
+    else:
+        return []
+
+def all_results_by_cn(cn:str, search_base:str =None, options:dict =None):
+    searcher = SearchBase(search_base,options)
+    searcher.execute_query(where_clause=("CN = '%s'" % cn), type="GC")
+    if searcher.row_count > 0:
+        return [result['distinguishedName'] for result in searcher.get_all_results()]
+    else:
+        return []
+
+def all_results_by_upn(upn:str, search_base:str =None, options:dict =None):
+    searcher = SearchBase(search_base,options)
+    searcher.execute_query(where_clause=("userPrincipalName = '%s'" % upn), type="GC")
+    if searcher.row_count > 0:
+        return [result['distinguishedName'] for result in searcher.get_all_results()]
+    else:
+        return []
 
 def all_results_by_sid(sid, search_base=None, options={}):
-    if not search_base:
-        if not ADBase.default_domain:
-            raise Exception("Unable to detect default domain. Must specify search base.")
-        search_base = ADBase.default_domain
-    _ad_query_obj.reset()
-    _ad_query_obj.execute_query(where_clause=("objectSid = '%s'" % sid), 
-        base_dn=search_base, 
-        options=options, 
-        type="GC")
-    return [result['distinguishedName'] for result in _ad_query_obj.get_all_results()]
+    searcher = SearchBase(search_base,options)
+    searcher.execute_query(where_clause=("objectSid = '%s'" % sid), type="GC")
+    if searcher.row_count > 0:
+        return [result['distinguishedName'] for result in searcher.get_all_results()]
+    else:
+        return []
