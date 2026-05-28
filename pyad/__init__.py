jcarswell/pyad

commit bb8e79b8d394d90a8fd0814b3f576f4a76065e9a

change(log): use logging instead of print to allow upper packages manage the messaging level

pyad/__init__.py
@@ -1,3 +1,7 @@
+# package logger
+import logging
+logging.basicConfig(level=logging.WARNING)
+
 __all__ = [
     "set_defaults",
     "ADQuery",

pyad/adbase.py
@@ -1,17 +1,20 @@
+import logging
 import sys
 import win32com.client
 
 from .pyadexceptions import SetupError
 
+logger = logging.getLogger(__name__)
 _adsi_provider = win32com.client.Dispatch("ADsNameSpaces")
 
 try:
     # Discover default domain and forest information
     __default_domain_obj = _adsi_provider.GetObject("", "LDAP://rootDSE")
 except:
-    # If there was an error, this this computer might not be on a domain.
-    print(
-        "WARN: unable to connect to default domain. Computer is likely not attached to an AD domain"
+    # If there was an error, this computer might not be on a domain.
+    logger.info(
+        "Unable to connect to default domain. "
+        "Computer is likely not attached to an AD domain."
     )
     __default_domain_obj = None
     _default_detected_forest = None

pyad/adobject.py
@@ -1,3 +1,4 @@
+import logging
 import win32com
 import pywintypes
 import time
@@ -16,6 +17,7 @@ from .pyadconstants import (
     ADS_USER_FLAG,
 )
 
+logger = logging.getLogger(__name__)
 
 @total_ordering
 class ADObject(ADBase):
@@ -664,7 +666,7 @@ class ADObject(ADBase):
                             )
                         node.appendChild(text)
                     except:
-                        print("attribute: %s not xml-able" % attribute)
+                        logger.error("attribute: %s not xml-able" % attribute)
                 else:
                     node.setAttribute("type", "multiValued")
                     ok_elem = False
@@ -684,7 +686,7 @@ class ADObject(ADBase):
                                 node.appendChild(valnode)
                                 ok_elem = True
                     except:
-                        print("attribute: %s not xml-able" % attribute)
+                        logger.error("attribute: %s not xml-able" % attribute)
                 if ok_elem:
                     adobj_xml_doc.appendChild(node)
         return doc.toxml(encoding="UTF-8")

commit 2788f1c62a77c85482d7dc42d151bb576c342456

fix(logging): logging.basicConfig was obfuscating downstream log configs
Bug introduced with bb8e79b8d394d90a8fd0814b3f576f4a76065e9a

Funded by Oslandia for https://github.com/qgis-deployment/qgis-deployment-toolbelt-cli project

pyad/__init__.py
@@ -1,6 +1,3 @@
-# package logger
-import logging
-logging.basicConfig(level=logging.WARNING)
 
 __all__ = [
     "set_defaults",
@@ -69,3 +66,8 @@ from .pyadexceptions import (
     noExecutedQuery,
     invalidResults,
 )
+
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.WARNING)